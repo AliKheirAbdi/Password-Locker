de8d7898a2198f8ed8f755108bd01d730e6dc0ff
AliKheir <akheirali@gmail.com>
2019-10-05 21:48:02 +0300

    created credentials class in credentials.py

---
+++ credentials.py
@@ -0,0 +1,10 @@
+class Credentials:
+    '''
+    class that generates instances of new credentials
+    '''
+    app_details = []
+    
+    def __init__(self,app,app_password):
+        self.app = app
+        self.app_password = app_password
+    

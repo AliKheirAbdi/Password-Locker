a28140b2a9320e4aefbaa05019df87da1c2b741d
AliKheir <akheirali@gmail.com>
2019-10-06 11:00:07 +0300

    this commit ass pyfiglet art to app

--- user-test.py
+++ user-test.py
@@ -1,35 +1,42 @@
 import unittest
 from user import User
+from credentials import Credentials
 
 class TestUser(unittest.TestCase):
     '''
     Test class that defines test cases for the user class behaviours.
     '''
     new_user = User("Ali","Kheir","Kh@1234") # captures user details
-   
+    new_app = Credentials("Facebook","Kh@1234") # captures app details from credentials class
     def setUp(self):
         '''
         setup method that runs before each test cases
         '''
         self.new_user = User("Ali","Kheir","Kh@1234")
+        self.new_app = Credentials("Facebook","Kh@1234")
 
     def tearDown(self):
         '''
         teardown method that clean up after each test case has run
         '''
         User.user_details = []
+        Credentials.app_details = []
 
 
     # test to check correct initialization of user
     def test_init(self):
         '''
-        test to check whether user initialized correctly
+        test to check whether user is initialized correctly
         '''
         self.assertEqual(self.new_user.first_name,"Ali")
         self.assertEqual(self.new_user.second_name,"Kheir")
         self.assertEqual(self.new_user.password,"Kh@1234")
 
-# test to check whether our user details have been saved
+        self.assertEqual(self.new_app.app,"Facebook")
+        self.assertEqual(self.new_app.app_password,"Kh@1234")
+
+
+    # test to check whether our user details have been saved
     def test_save_user(self):
         '''
         test that checks whether our user details have been saved
@@ -37,16 +44,9 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user()
         self.assertEqual(len(User.user_details), 1)
 
-self.new_user.save_user()
-        self.assertEqual(len(User.user_details),1)
+
     # deleting user
     def test_delete_user(self):
-        '''
-        test to check whether a user can delete account
-        '''
-        User.user_details.remove(self)
-
-    test_delete_user(self):
         '''
         test checks whether a user can delete their account
         '''
@@ -55,9 +55,6 @@ self.new_user.save_user()
         test_user.save_user
 
         self.new_user.delete_account()
-        self.assertEqual(len(User.user_details),0)
-
-self.new_user.delete_account()
         self.assertEqual(len(User.user_details),0)
 
 
@@ -74,8 +71,24 @@ self.new_user.delete_account()
 
         self.assertTrue(user_exists)
 
-            found_user = User.find_by_fname("Ali")
+    # test that finds user by first_name
+    def test_find_user_by_firstname(self):
+        '''
+        test to find user by first name
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Ali","Kheir","Kh@1234")
+        test_user.save_user()
+
+        found_user = User.find_by_fname("Ali")
 
         self.assertEqual(found_user.first_name,test_user.first_name)
+    # test that displays users
+    def display_user(self):
+        '''
+        Test that displays users
+        '''
+        self.assertEqual(User.display_user(),User.user_details)
 if __name__ == '__main__':
     unittest.main()
--- user.py
+++ user.py
@@ -1,94 +1,62 @@
-import unittest
-from user import User
-from credentials import Credentials
+class User:
+    """
+    class that generates new user
+    """
+    user_details = []
 
-class TestUser(unittest.TestCase):
-    '''
-    Test class that defines test cases for the user class behaviours.
-    '''
-    new_user = User("Ali","Kheir","Kh@1234") # captures user details
-    new_app = Credentials("Facebook","Kh@1234") # captures app details from credentials class
-    def setUp(self):
-        '''
-        setup method that runs before each test cases
-        '''
-        self.new_user = User("Ali","Kheir","Kh@1234")
-        self.new_app = Credentials("Facebook","Kh@1234")
+    def __init__(self, first_name, second_name, password):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.password = password
+    # This saves a new user registration
 
-    def tearDown(self):
-        '''
-        teardown method that clean up after each test case has run
-        '''
-        User.user_details = []
-        Credentials.app_details = []
+    def save_user(self):
 
+        """
+        this save method adds and stores our user
+        """
+        User.user_details.append(self)
 
-    # test to check correct initialization of user
-    def test_init(self):
-        '''
-        test to check whether user is initialized correctly
-        '''
-        self.assertEqual(self.new_user.first_name,"Ali")
-        self.assertEqual(self.new_user.second_name,"Kheir")
-        self.assertEqual(self.new_user.password,"Kh@1234")
+    # deleting the user account
 
-        self.assertEqual(self.new_app.app,"Facebook")
-        self.assertEqual(self.new_app.app_password,"Kh@1234")
+    def delete_account(self):
+        """
+        delete account method to remove user account
+        """
+        User.user_details.remove(self)
 
+    # finding user by first_name
+    @classmethod
+    def find_by_fname(cls, first_name):
+        """
+        find user by their first name
+        Args:
+            first_name: first name of the user to search for
+        returns:
+            user searched for
+        """
+        for user in cls.user_details:
+            if user.first_name == first_name:
+                return user
 
-    # test to check whether our user details have been saved
-    def test_save_user(self):
-        '''
-        test that checks whether our user details have been saved
-        '''
-        self.new_user.save_user()
-        self.assertEqual(len(User.user_details), 1)
+    # confirm user exist
+    @classmethod
+    def user_exist(cls, first_name):
+        """
+        method that checks if the user exists by name
+        Args:
+        first_name checks if the name exists
+        returns a boolean depending if the name exists
+        """
+        for user in cls.user_details:
+            if user.first_name == first_name:
+                return True
+        return False
+    # display user
+    @classmethod
+    def display_user(cls):
+        """
+        Function that displays users 
+        """
 
-
-    # deleting user
-    def test_delete_user(self):
-        '''
-        test checks whether a user can delete their account
-        '''
-        self.new_user.save_user()
-        test_user = User("Ali", "Kheir", "Kh@1234")
-        test_user.save_user
-
-        self.new_user.delete_account()
-        self.assertEqual(len(User.user_details),0)
-
-
-    # test checking if user exists
-    def test_user_exists(self):
-        '''
-        checks if user exists by password. returns a boolean if user doesn't exist
-        '''
-        self.new_user.save_user()
-        test_user = User("Kheir", "Testsecondname", "Kh@1234")
-        test_user.save_user()
-
-        user_exists = User.user_exist("Ali")
-
-        self.assertTrue(user_exists)
-
-    # test that finds user by first_name
-    def test_find_user_by_firstname(self):
-        '''
-        test to find user by first name
-        '''
-
-        self.new_user.save_user()
-        test_user = User("Ali","Kheir","Kh@1234")
-        test_user.save_user()
-
-        found_user = User.find_by_fname("Ali")
-
-        self.assertEqual(found_user.first_name,test_user.first_name)
-    # test that displays users
-    def display_user(self):
-        '''
-        Test that displays users
-        '''
-        self.assertEqual(User.display_user(),User.user_details)
-if __name__ == '__main__':
-    unittest.main()
+        return cls.user_details()

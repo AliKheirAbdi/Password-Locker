743cd468cb4d2b6f1891ca7e1cd3da03bb033b1b
AliKheir <akheirali@gmail.com>
2019-10-06 09:32:11 +0300

    this commit tests password gen to the credentials

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
+import random
 import string
 
 class Credentials:
@@ -21,3 +22,53 @@ class Credentials:
         '''
         Credentials.app_details.remove(self)
     
+    @classmethod
+    def find_app(cls,app):
+        '''
+        Finding app by the name
+        Args:
+            app: app name to search for
+        returns:
+            app searched for
+        '''
+        for credentials in cls.app_details:
+            if credentials.app == app:
+                return app
+    
+    @classmethod
+    def app_exist(cls,app):
+        '''
+        Method that checks if app exist
+        Args:
+            app name to check if app exist
+        Returns:
+            a boolean depending on the app checked for
+        '''
+        for credentials in cls.app_details:
+            if credentials.app == app:
+                return True
+        return False
+
+    @classmethod
+    def gen_password(self):
+        '''
+        Function to generate random password with six digits
+        '''
+        char = string.ascii_uppercase + string.ascii_lowercase
+        gen_password = ''.join(random.choice(char) for i in range(0, 9))
+        return gen_password
+
+    @classmethod
+    def display_app(cls, app):
+        '''
+        Function that displays app
+        '''    
+        
+        return cls.app_details()
+
+
+
+
+
+
+
